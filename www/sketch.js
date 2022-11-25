96c02fd51e7924effbc898e69791f9678552f6f3
nadanoda <s00528@naver.com>
2022-11-25 16:54:59 +0900

    11.25
    - 4시 15분까지활동

--- www/sketch.js
+++ www/sketch.js
@@ -1,7 +1,29 @@
+var wave;
+var playing = false;
+var button;
 function setup() {
-  createCanvas(400, 400);
+  //createCanvas(displayWidth, displayHeight);
+  createCanvas(600,480);
+  wave = new p5.Osciliator();
+  wave.setType('sine');
+  wave.start();
+  wave.freq(440);
+  wave.amp(0);
+
+  button = createButton('play/pause');
+  button.mousePressed(toggle);
 }
 
 function draw() {
-  background(220);
+}
+
+function toggle(){
+  if(!playing){
+    wave.amp(0.5,1);
+    playing = true;
+  }
+  else{
+    wave.amp(0,1);
+    playing= false;
+  }
 }
