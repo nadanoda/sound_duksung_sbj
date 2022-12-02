f355a5ad092533ed7bf7a335ff609d3fdc3f6d4e
nadanoda <s00528@naver.com>
2022-12-02 15:42:15 +0900

    12.2
    가속도

---
+++ sketch - 복사본.js
@@ -0,0 +1,31 @@
+var wave;
+var button;
+var playing = false;
+
+function setup() {
+  //createCanvas(displayWidth, displayHeight);
+  createCanvas(720,256);
+  wave = new p5.Oscillator();
+  wave.setType('sine');
+
+  wave.freq(440);
+  wave.amp(0);
+
+  button=createButton('play/pause');
+  button.mousePressed(toggle);
+}
+
+function draw() {
+}
+
+function toggle(){
+  if(!playing){
+    wave.start();
+    wave.amp(0.5,1);
+    playing = true;
+  }
+  else{
+    wave.amp(0,1);
+    playing= false;
+  }
+}
