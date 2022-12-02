046fb3d6b45f70dcd108f05d5c12283069052b0a
nadanoda <s00528@naver.com>
2022-12-02 16:10:58 +0900

    12.2
    가속도 숫자?

--- sketch.js
+++ sketch.js
@@ -3,18 +3,23 @@ let r,g,b;
 function setup() {
   //createCanvas(displayWidth, displayHeight);
   createCanvas(displayWidth, displayHeight);
-  r=random(50,255);
-  g=random(0,200);
-  b=random(50,255);
+  //r=random(50,255);
+  //g=random(0,200);
+  //b=random(50,255);
 }
 
 function draw() {
-  background(r,g,b);
-  console.log('draw');
+  //background(r,g,b);
+  //console.log('draw');
+  //text("accelerationX+")
 }
 
-function toggle(){
-  r=map(accelerationX,-90,90,100,175);
-  g=map(accelerationX,-90,90,100,200);
-  b=map(accelerationX,-90,90,100,200);
+//function toggle()
+function deviceMoved()
+{
+  background(accelerationX, accelerationY, accelerationZ);
+  text(accelerationX+", "+ accelerationY+","+accelerationZ, 10 , 10);
+  //r=map(accelerationX,-90,90,100,175);
+  //g=map(accelerationX,-90,90,100,200);
+  //b=map(accelerationX,-90,90,100,200);
 }
