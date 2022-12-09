a21b5eb3f921de48b6053589d9c2c7cf6b068890
nadanoda <s00528@naver.com>
2022-12-09 15:35:50 +0900

    사운드아트
    아이폰도 화면 변경가능

--- sketch.js
+++ sketch.js
@@ -1,25 +1,36 @@
-let r,g,b;
+let button; //센서 허용을 위한 버튼
+let permission = false; // 허가를 위한 변수 (기본은 허가가 안남)
 
-function setup() {
-  //createCanvas(displayWidth, displayHeight);
-  createCanvas(displayWidth, displayHeight);
-  //r=random(50,255);
-  //g=random(0,200);
-  //b=random(50,255);
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  // 윈도우 사이즈로 displayWidth displayHeight가능
+  
+  if(typeof DeviceMotionEvent.requestPermission === "function"){
+    //최신 ios인 경우
+    background(255,0,0); //붉은 배경
+    button = creatButton("Click to iOS Sensor"); // 센서허용을 위한 변수
+    button.mousePressed(iosAccess); // 버튼 누르면 iosAccess 함수로
+  }else{
+    // 최신 iOS가 아닌 다른 os 예: iOS저버전, 안드로이드, 윈도우, 맥
+    background(0,255,0); // 녹색배경
+    text("is not a ios", 100, 100); // 글자로 100, 100위치에 is not  a ios라고 씀
+  }
 }
 
-function draw() {
-  //background(r,g,b);
-  //console.log('draw');
-  //text("accelerationX+")
+function iosAccess(){//버튼 누르면 동작하는 함수(그냥 따라하기)
+  DeviceOrientationEvent.requestPermission()
+    .then((response) => {
+    if(response === "granted"){ //granted를 받으면
+      permission = true; // 허가가 true(허가남)
+    }
+  })
+  .catch(consloe.error);
 }
 
-//function toggle()
-function deviceMoved()
-{
-  background(accelerationX, accelerationY, accelerationZ);
-  text(accelerationX+", "+ accelerationY+","+accelerationZ, 10 , 10);
-  //r=map(accelerationX,-90,90,100,175);
-  //g=map(accelerationX,-90,90,100,200);
-  //b=map(accelerationX,-90,90,100,200);
+function draw(){
+  if(!permission)return; // 허가 안나면 리턴
+  background(255,255,255); // 흰색배경
+  textSize(72); // 텍스트 픽셀 크기 72
+  text(rotationX, 100, 100); // 텍스트는 x회전값이고, 100 100 위치에 글자로 표기
 }
+
